3cc4985c7f767ed72f13120ef8f06837c9f85dda
Руслан Исламов <r.islamov@@edu.centraluniversity.ru>
2025-06-07 09:08:02 +0300

    DSN to env + limiter

--- auth/go-auth-service/cmd/main.go
+++ auth/go-auth-service/cmd/main.go
@@ -4,13 +4,48 @@ import (
 	"auth-service/internal/handlers"
 	"auth-service/internal/middleware"
 	"auth-service/internal/storage"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/httprate"
 )
 
+type dbDSN struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func getDSN(prefix string) string {
+	cfg := dbDSN{
+		Host:     os.Getenv(prefix + "_HOST"),
+		Port:     os.Getenv(prefix + "_PORT"),
+		User:     os.Getenv(prefix + "_USER"),
+		Password: os.Getenv(prefix + "_PASSWORD"),
+		DBName:   os.Getenv(prefix + "_NAME"),
+		SSLMode:  os.Getenv(prefix + "_SSLMODE"),
+	}
+
+	if cfg.Host == "" || cfg.Port == "" || cfg.User == "" || cfg.Password == "" || cfg.DBName == "" {
+		log.Fatalf("Missing required environment variables for %s", prefix)
+	}
+	if cfg.SSLMode == "" {
+		cfg.SSLMode = "disable"
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+}
+
 func main() {
-	authDSN := "postgres://user:pass@db-auth:5432/auth?sslmode=disable"
-	bookingDSN := "postgres://user:pass@db-booking:5432/booking?sslmode=disable"
+	authDSN := getDSN("AUTH_DB")
+	bookingDSN := getDSN("BOOKING_DB")
 	store, err := storage.New(authDSN, bookingDSN)
 	if err != nil {
 		log.Fatal(err)
@@ -18,11 +53,14 @@ func main() {
 
 	h := &handlers.AuthHandler{Store: store}
 
+	// лимитер для http запросов (не более 5 в минуту) => БД в безопасности
+	limiter := httprate.LimitByIP(5, 1*time.Minute)
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/register", h.Register)
-	mux.HandleFunc("/login", h.Login)
-	mux.Handle("/me", middleware.JWTAuth(http.HandlerFunc(h.Me)))
-	mux.Handle("/logout", middleware.JWTAuth(middleware.RequireRole("admin", http.HandlerFunc(h.Logout))))
+	mux.Handle("/register", limiter(http.HandlerFunc(h.Register)))
+	mux.Handle("/login", limiter(http.HandlerFunc(h.Login)))
+	mux.Handle("/me", limiter(middleware.JWTAuth(http.HandlerFunc(h.Me))))
+	mux.Handle("/logout", limiter(middleware.JWTAuth(middleware.RequireRole("admin", http.HandlerFunc(h.Logout)))))
 
 	handler := middleware.CORS(mux)
 
